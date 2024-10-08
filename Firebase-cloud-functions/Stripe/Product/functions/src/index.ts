f01a7df994862e2f9f6155fdb5d16d1e52b6ac60
Jonathan Svitana <jonathansvitana@gmail.com>
2024-10-07 21:42:36 -0400

    Add create product functionality

--- Firebase-cloud-functions/Stripe/Product/functions/src/index.ts
+++ Firebase-cloud-functions/Stripe/Product/functions/src/index.ts
@@ -1,19 +1,39 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { requestCreateProduct } from "./models/request-create-product";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const createProduct = onRequest(async (request, response) => {
+  logger.info("Starting createProduct firebase cloud function", {structuredData: true});
 
-export const helloWorldd = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  const body: requestCreateProduct = request.body;
+
+  // REMOVE THIS KEY, PUT IN A SETTINGS FILE
+  const stripe = require('stripe')('sk_test_51MXs4ABKPh6RnjBZlXPNd7dztzgJAw8LjIQaWhXqdid6sWEc2ICcqZPoRODXYJhSx3zpY2223px891pGFOuuCNUU00BIVhUitz');
+
+  logger.info("Attempting create product name: " + body.name)
+
+  // TEST THE ERROR HANDLING OF THIS
+  try {
+    const product = await stripe.products.create({
+      name: body.name,
+      description: body.description,
+      default_price_data: {
+        currency: 'USD',
+        unit_amount: body.centPrice
+      },
+      metadata: {
+        deerId: body.deerId,
+        ranchId: body.ranchId
+      }
+    });
+
+    logger.info("Successful creation of product name: " + body.name)
+  
+    response.send(product)
+  } catch(ex) {
+    logger.info("Failed to create product name: " + body.name)
+    logger.info("Exception: " + ex)
+
+    response.sendStatus(500);
+  }
 });

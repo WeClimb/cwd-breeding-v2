df165b0159469a413e8ebf16a1f1206b73800e90
Jonathan Svitana <jonathansvitana@gmail.com>
2025-01-20 17:45:13 -0500

    Finish first pass at session creation

--- Firebase-cloud-functions/Stripe/Session-create/functions/src/index.ts
+++ Firebase-cloud-functions/Stripe/Session-create/functions/src/index.ts
@@ -1,7 +1,51 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import {sessionCreate} from "./models/session-create.mode";
+import {defineSecret} from "firebase-functions/params";
 
-export const createStripeSession = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const stripeAPIKey = defineSecret("STRIPEAPIKEY");
+
+export const createStripeSession = onRequest({
+  secrets: [stripeAPIKey],
+},
+async (request, response) => {
+  logger.info("Starting session create function", {structuredData: true});
+
+  if (!request.body || !request.body.productId) {
+    logger.info("Request did not include body or a product ID", {structuredData: true});
+
+    response.status(400).send("Request body or Product ID missing");
+  } else {
+    const body: sessionCreate = {
+      productIds: request.body.productId,
+    };
+
+    const stripe = require("stripe")(stripeAPIKey.value());
+
+    try {
+      const lineItems = [];
+
+      for (const productId of body.productIds) {
+        const product = await stripe.products.retrieve(productId);
+
+        lineItems.push({
+          price: product.default_price,
+          quantity: 1,
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        success_url: "https://example.com/success",
+        cancel_url: "https://example.com/cancel",
+        line_items: lineItems,
+        mode: "subscription",
+      });
+
+      logger.info("Session creation success. Session ID: " + session.id, {structuredData: true});
+
+      response.status(200).send(session.url);
+    } catch (ex) {
+      response.status(400).send(ex);
+    }
+  }
 });
